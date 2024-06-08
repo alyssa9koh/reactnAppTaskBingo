0f0822504536a38194f810976e02465c4d36706e
Alyssa Koh <a9koh@stanford.edu>
2024-06-07 21:10:23 -0700

    you can change the task on a square now by holding on it

--- src/components/BingoBoard/BingoBoard.tsx
+++ src/components/BingoBoard/BingoBoard.tsx
@@ -1,8 +1,26 @@
 import React, { useState } from 'react';
 import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import Dialog from "react-native-dialog";
 
 function BingoSquare() {
     const [pressed, setPressed] = useState(false);
+    const [task, setTask] = useState('default task');
+
+    const [visible, setVisible] = useState(false);
+    const [inputValue, setInputValue] = useState(task);
+
+    const showDialog = () => {
+        setVisible(true);
+    };
+
+    const handleCancel = () => {
+        setVisible(false);
+    };
+
+    const handleChange = () => {
+        setTask(inputValue);
+        setVisible(false);
+    }
 
     return (
         <TouchableOpacity
@@ -11,12 +29,27 @@ function BingoSquare() {
             onPress={() => {
                 setPressed(!pressed);
             }}
+            onLongPress={()=>{
+                showDialog();
+            }}
         >
             <Text 
                 style={[styles.bingoSquareText, pressed && styles.bingoSquareTextPressed]}
             >
-                I am bingo square
+                {task}
             </Text>
+            <Dialog.Container visible={visible}>
+                <Dialog.Title>
+                    Change Task
+                </Dialog.Title>
+                <Dialog.Input
+                    placeholder="Type here"
+                    onChangeText={(text) => setInputValue(text)}
+                    value={inputValue}
+                />
+                <Dialog.Button label="Cancel" onPress={handleCancel} />
+                <Dialog.Button label="Change" onPress={handleChange} />
+            </Dialog.Container>
         </TouchableOpacity>
     )
 }
