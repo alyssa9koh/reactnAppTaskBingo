5f7a5c9b1d02361fba3fa0f5464995a6a49cd1d1
Alyssa Koh <a9koh@stanford.edu>
2024-06-10 13:21:04 -0700

    very basic board creationform

--- src/screens/AddBoardScreen.tsx
+++ src/screens/AddBoardScreen.tsx
@@ -1,30 +1,85 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { Button, StyleSheet, View, Text, TextInput } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Slider from '@react-native-community/slider';
 
+import { MIN_SIZE, MAX_SIZE, DEFAULT_SIZE } from '../utils/defaults';
+
 export default function AddBoardScreen({ navigation }) {
+    const [textInput, setTextInput] = useState('My Bingo Board');
+    const [sizeInput, setSizeInput] = useState(DEFAULT_SIZE);
+
+    function handleTextChange(value) {
+        setTextInput(value);
+    }
+
+    function handleValueChange(value) {
+        setSizeInput(value);
+    }
+
+    function handleCreateBoard() {
+        console.log(`i cooka da new board`);
+        console.log(`new board name: ${textInput}`);
+        console.log(`new board size: ${sizeInput}`);
+    }
+    
     return (
         <View style={styles.addBoardScreen}>
-            <Slider
-                style={styles.addBoardStyle}
-                minimumValue={3}
-                maximumValue={6}
-                minimumTrackTintColor="black"
-                maximumTrackTintColor="black"
+            <TextInput
+                style={[styles.textInput, styles.text]}
+                placeholder="Enter board name"
+                value={textInput}
+                onChangeText={handleTextChange}
+            />
+            <View style={styles.sizeSelect}>
+                <Text style={styles.text}>{`${sizeInput}x${sizeInput} board`}</Text>
+                <Slider
+                    style={styles.sizeSlider}
+                    minimumValue={MIN_SIZE}
+                    maximumValue={MAX_SIZE}
+                    step={1}
+                    value={DEFAULT_SIZE}
+                    minimumTrackTintColor="black"
+                    maximumTrackTintColor="black"
+                    onValueChange={handleValueChange}
+                />
+            </View>
+            <Button 
+                title={'Create Board'} 
+                onPress={handleCreateBoard}
             />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    text: {
+        fontSize: 17.5
+    },
     addBoardScreen: {
         height: '100%',
         alignItems: 'center',
         justifyContent: 'center',
         paddingBottom: '20%'
     },
-    addBoardStyle: {
-        width: '80%'
+    textInput: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 10,
+        width: '75%',
+        textAlign: 'center',
+        backgroundColor: 'white'
+    },
+    sizeSelect: {
+        margin: 25,
+        flexDirection: 'row',
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    sizeSlider: {
+        width: '60%',
+        padding: 10
     }
 });
--- src/utils/defaults.ts
+++ src/utils/defaults.ts
@@ -1,1 +1,4 @@
 export const DEFAULT_SIZE = 5;
+
+export const MIN_SIZE = 3;
+export const MAX_SIZE = 6;
