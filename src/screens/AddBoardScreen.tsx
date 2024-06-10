cb985e430c431b35aa1a937fba08607682725468
Alyssa Koh <a9koh@stanford.edu>
2024-06-10 02:16:42 -0700

    import

--- src/screens/AddBoardScreen.tsx
+++ src/screens/AddBoardScreen.tsx
@@ -1,4 +1,5 @@
 import { StyleSheet, View, Text } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function AddBoardScreen({ navigation }) {
     return (
