47fb7aaab46acd30794f18f1f0baa92fae956129
Alyssa Koh <a9koh@stanford.edu>
2024-06-10 01:30:39 -0700

    changed screen stuff; moved mock data to home screen

--- src/screens/BoardScreen.tsx
+++ src/screens/BoardScreen.tsx
@@ -2,20 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Button, Text, View } from 'react-native';
 
 import BingoBoard from '../components/BingoBoard/BingoBoard';
+import { DEFAULT_SIZE } from '../utils/defaults';
 
-export default function BoardScreen({ navigation }) {
-    const defaultTitle = 'My Bingo Board';
-    const initialSize = 5;
-    const initialTasks = Array(initialSize ** 2).fill('default task');
-    const initialPressedSquares = Array(initialSize ** 2).fill(false);
+export default function BoardScreen({ route }) {
+    const {
+        title = 'My Bingo Board',
+        tasks = Array(DEFAULT_SIZE ** 2).fill('default task'),
+        pressedSquares = Array(DEFAULT_SIZE ** 2).fill(false)
+    } = route.params || {};
 
     return (
         <View style={styles.container}>
             <BingoBoard 
-                size={initialSize} 
-                initialTitle={defaultTitle} 
-                initialTasks={initialTasks} 
-                initialPressedSquares={initialPressedSquares} 
+                size={DEFAULT_SIZE} 
+                initialTitle={title} 
+                initialTasks={tasks} 
+                initialPressedSquares={pressedSquares} 
                 initialPressedCount={0}
             />
             <StatusBar style="auto" />
@@ -25,9 +27,9 @@ export default function BoardScreen({ navigation }) {
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center'
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
     },
-  });  
+});  
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,20 @@
 import { Button, Text, View } from 'react-native';
 
+import { DEFAULT_SIZE } from '../utils/defaults';
+
+const mock_1 = {
+    title: 'My Bingo Board',
+    size: DEFAULT_SIZE,
+    tasks: Array(DEFAULT_SIZE ** 2).fill('default task'),
+    pressedSquares: Array(DEFAULT_SIZE ** 2).fill(false)
+};
+
 export default function HomeScreen({ navigation }) {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Home Screen</Text>
             <Button
                 title="Go to Bingo Board"
-                onPress={() => navigation.navigate('Board')}
+                onPress={() => navigation.navigate('Board', mock_1)}
             />
         </View>
     );
